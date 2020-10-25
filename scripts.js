6a4c38efc6d62806bfc5f446ca5b00a5d6f7abbc
Jón Guðjónsson <jongudjonsson@gmail.com>
2020-10-25 23:33:39 +0000

    Foll tilbuin

--- scripts.js
+++ scripts.js
@@ -8,12 +8,12 @@ const LETTERS = `AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ`;
  * Byrja forrit.
  */
 function start() {
-  alert('Halló!')
+  //alert('Halló!')
+  console.log(encode('A', 3));
 }
 
 // Hér er gott að commenta út til að vinna í encode/decode föllum fyrst og síðan „viðmóti“ forrits
 start();
-
 /**
  * Kóðar streng með því að hliðra honum um n stök.
  *
@@ -22,7 +22,7 @@ start();
  * @returns {string} Upprunalegi strengurinn hliðraður um n til hægri
  */
 function encode(str, n) {
-  return str;
+  return codeSolver(str, LETTERS, shift(n));
 }
 
 /**
@@ -33,7 +33,40 @@ function encode(str, n) {
  * @returns {string} Upprunalegi strengurinn hliðraður um n til vinstri
  */
 function decode(str, n) {
-  return str;
+  return codeSolver(str, shift(n), LETTERS);
+}
+
+function shift(n){
+  // Tekur módulus til að virki líka á n > 31.
+  let shift = n%32;
+  return LETTERS.substring(shift) + LETTERS.substring(0, shift);
+}
+
+function codeSolver(str, keyOne, keyTwo){
+  let solvedStr = "";
+  str = str.toLocaleUpperCase();
+  for(let i = 0; i < str.length; i++){
+    for(let n = 0; n < keyOne.length; n++){
+      if(str.charAt(i) === keyOne.charAt(n)){
+        solvedStr += keyTwo.charAt(n);
+      }
+    }
+  }
+  return solvedStr;
+ /*
+
+  while(str.length){
+    let keyOneTemp = keyOne;
+    while(keyOneTemp.length){
+      if(str.charAt(0) == keyOneTemp.charAt(0)){
+        solvedStr += keyTwo.charAt(0);
+      }
+      keyOneTemp = keyOneTemp.substring(1);
+    }
+    str = str.substring(1);
+  }
+  return solvedStr;
+  */
 }
 
 console.assert(encode('A', 3) === 'D', 'kóðun á A með n=3 er D');
