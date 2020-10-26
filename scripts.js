6fa7da9a7e9a053ccf86868b0a44c2f370239c0d
Jón Guðjónsson <jongudjonsson@gmail.com>
2020-10-26 21:25:13 +0000

    Tilbuid

--- scripts.js
+++ scripts.js
@@ -9,26 +9,60 @@ const LETTERS = `AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ`;
  */
 function start() {
   while(1){
+
     let action = prompt("Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða");
-    if(action !== "kóða" && action !== "afkóða"){
+    if(!action){
+      break;
+    }else if(action !== "kóða" && action !== "afkóða"){
       alert(`Veit ekki hvaða aðgerð „${action}“ er. Reyndu aftur.`);
       continue
     }
-    let n = prompt("Hversu mikið á að hliðra streng? Gefðu upp heiltölu á bilinu [1, 31]");
-    if(!Number.isInteger(n) || (Number.parseInt(n) > 32 || Number.parseInt(n) < 0){
-      alert(`${n} er ekki heiltala á bilinu [1, 31]. Reyndu aftur.`)
-      continue
-    }
-    let str = prompt(`Gefðu upp strenginn sem á að ${action} með hliðrun ${n}:`);
-    if(typeof str !== 'string')
 
-  confirm(
+    let n = prompt("Hversu mikið á að hliðra streng? Gefðu upp heiltölu á bilinu [1, 31]");
+    if(!n){
+      break;
+    }else{
+      n = Number.parseInt(n)
+      if(!Number.isInteger(n) || (n > 31 || n < 1)){
+        alert(`${n} er ekki heiltala á bilinu [1, 31]. Reyndu aftur.`)
+        continue
+      }
+    }
+
+    let str = prompt(`Gefðu upp strenginn sem á að ${action} með hliðrun ${n}:`);
+    if(!str){
+      break;
+    }else if(typeof str !== 'string'){
+      alert(`Þú gafst ekki upp streng. Reyndu aftur.`)
+      continue;
+    }else{
+      str = str.toLocaleUpperCase();
+      let invalid = [];
+      for(let i = 0; i < str.length; i++){
+        if(!LETTERS.includes(str.charAt(i))){
+          invalid.push(str.charAt(i));
+        }
+      }
+      if(invalid.length){
+        alert(`Þú gafst upp stafi sem ekki er hægt að ${action}: ${invalid.join(', ')}.`)
+        continue;
+      }
+    }
+
+    if(action === "kóða"){
+      lausn = encode(str, n);
+    }else{
+      lausn = decode(str, n);
+    }
+
+    alert(`${str} ${action}st sem: ${lausn}.`)
+
+    if(confirm(`Viltu byrja upp á nýtt?`)){
+      continue;
+    }
+
     break;
   }
-
-//Number.parseInt()
-//Number.isInteger()
-
 }
 
 // Hér er gott að commenta út til að vinna í encode/decode föllum fyrst og síðan „viðmóti“ forrits
@@ -55,6 +89,12 @@ function decode(str, n) {
   return codeSolver(str, shift(n), LETTERS);
 }
 
+/**
+ * Hliðrar streng um n stök.
+ *
+ * @param {number} n Hliðrun, heiltala stærri en 0
+ * @returns {string} Upprunalegi strengurinn hliðraður um n til vinstri
+ */
 function shift(n){
   // Tekur módulus til að virki líka á n > 31 (væri því hægt
   // að sleppa kröfu um að talan sé á bilinu [1, 31]).
@@ -62,9 +102,16 @@ function shift(n){
   return LETTERS.substring(shift) + LETTERS.substring(0, shift);
 }
 
+/**
+ * Afkóðar streng með því víxla stöfum útfrá tveimur dulkóðslyklum.
+ *
+ * @param {string} str Strengur sem skal afkóða, aðeins stafir í stafrófi
+ * @param {string} keyOne Strengur með fyrri dulmálslykli, þarf að vera jafn langur og keyTwo
+ * @param {string} keyTwo Strengur með fyrri dulmálslykli, þarf að vera jafn langur og keyOne
+ * @returns {string} Upprunalegi strengurinn str dulkóðaður úr kóða keyOne í kóða keyTwo
+ */
 function codeSolver(str, keyOne, keyTwo){
   let solvedStr = "";
-  str = str.toLocaleUpperCase();
   for(let i = 0; i < str.length; i++){
     for(let n = 0; n < keyOne.length; n++){
       if(str.charAt(i) === keyOne.charAt(n)){
@@ -73,20 +120,6 @@ function codeSolver(str, keyOne, keyTwo){
     }
   }
   return solvedStr;
- /*
-
-  while(str.length){
-    let keyOneTemp = keyOne;
-    while(keyOneTemp.length){
-      if(str.charAt(0) == keyOneTemp.charAt(0)){
-        solvedStr += keyTwo.charAt(0);
-      }
-      keyOneTemp = keyOneTemp.substring(1);
-    }
-    str = str.substring(1);
-  }
-  return solvedStr;
-  */
 }
 
 console.assert(encode('A', 3) === 'D', 'kóðun á A með n=3 er D');
