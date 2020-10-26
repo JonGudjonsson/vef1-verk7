f893ba651cb1ce16f848e1e6642a04ea3456c3f2
Jón Guðjónsson <jongudjonsson@gmail.com>
2020-10-26 09:15:56 +0000

    Halfnadur med vidmotid

--- scripts.js
+++ scripts.js
@@ -8,8 +8,27 @@ const LETTERS = `AÁBDÐEÉFGHIÍJKLMNOÓPRSTUÚVXYÝÞÆÖ`;
  * Byrja forrit.
  */
 function start() {
-  //alert('Halló!')
-  console.log(encode('A', 3));
+  while(1){
+    let action = prompt("Hvort viltu kóða eða afkóða streng? Skrifaðu „kóða“ eða „afkóða");
+    if(action !== "kóða" && action !== "afkóða"){
+      alert(`Veit ekki hvaða aðgerð „${action}“ er. Reyndu aftur.`);
+      continue
+    }
+    let n = prompt("Hversu mikið á að hliðra streng? Gefðu upp heiltölu á bilinu [1, 31]");
+    if(!Number.isInteger(n) || (Number.parseInt(n) > 32 || Number.parseInt(n) < 0){
+      alert(`${n} er ekki heiltala á bilinu [1, 31]. Reyndu aftur.`)
+      continue
+    }
+    let str = prompt(`Gefðu upp strenginn sem á að ${action} með hliðrun ${n}:`);
+    if(typeof str !== 'string')
+
+  confirm(
+    break;
+  }
+
+//Number.parseInt()
+//Number.isInteger()
+
 }
 
 // Hér er gott að commenta út til að vinna í encode/decode föllum fyrst og síðan „viðmóti“ forrits
@@ -37,7 +56,8 @@ function decode(str, n) {
 }
 
 function shift(n){
-  // Tekur módulus til að virki líka á n > 31.
+  // Tekur módulus til að virki líka á n > 31 (væri því hægt
+  // að sleppa kröfu um að talan sé á bilinu [1, 31]).
   let shift = n%32;
   return LETTERS.substring(shift) + LETTERS.substring(0, shift);
 }
